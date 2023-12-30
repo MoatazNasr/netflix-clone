ffe03efb5704cd7a98be6d9329ec90ece4c5430d
MoatazNasr <75985257+MoatazNasr@users.noreply.github.com>
2023-12-30 19:47:57 +0200

    Update SearchResults.jsx

--- src/components/searchresults/SearchResults.jsx
+++ src/components/searchresults/SearchResults.jsx
@@ -8,7 +8,7 @@ const SearchResults = ({ query }) => {
   const [searchResults, setSearchResults] = useState([]);
   
   useEffect(() => {
-    axiosInstance.get(`/search/${movieOrtvState.movieOrtv}/?api_key=${process.env.REACT_APP_TMDB_KEY}&query=${query}`)
+    axiosInstance.get(`/search/${movieOrtvState.movieOrtv}?api_key=${process.env.REACT_APP_TMDB_KEY}&query=${query}`)
       .then((res) => {
         setSearchResults(res.data.results);
       })
